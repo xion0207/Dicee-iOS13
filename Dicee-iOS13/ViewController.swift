b25f5ebf0083d26e6467113867b50ec27d9ca514
松本詞音 <98865688+xion0207@users.noreply.github.com>
2022-10-17 20:50:10 +0900

    Initial Commit

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       //          WHO.WHAT = VALUE
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
 
